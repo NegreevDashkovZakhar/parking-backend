f9199204cbac442687dbdbd6585511695e57aa6f
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-15 21:57:14 +0600

    [+]Added car service implementation

---
+++ src/main/java/it/me/parking/service/CarService.java
@@ -0,0 +1,64 @@
+package it.me.parking.service;
+
+import it.me.parking.model.entity.Car;
+import it.me.parking.model.request.CarRequest;
+import it.me.parking.repository.CarRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Default car service interface implementation
+ *
+ * @see it.me.parking.service.ICarService
+ */
+@Service
+public class CarService implements ICarService {
+    private final CarRepository repository;
+
+    /**
+     * Constructor setting used car repository
+     *
+     * @param repository car repository used for manipulations
+     */
+    public CarService(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Iterable<Car> getAllCars() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Car getCarById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void updateCar(Long id, CarRequest newCarData) {
+        Car oldCar = repository.findById(id).orElseThrow();
+        oldCar.setModel(newCarData.getModel());
+        oldCar.setLicensePlateNumber(newCarData.getLicensePlateNumber());
+        oldCar.setOwnerName(newCarData.getOwnerName());
+        repository.save(oldCar);
+    }
+
+    @Override
+    public void deleteCarById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Car getCarByLicensePlateNumber(String licensePlateNumber) {
+        return null;
+//        return repository.getCarByLicensePlateNumber(licensePlateNumber);
+    }
+
+    @Override
+    public void addCar(CarRequest car) {
+        repository.save(new Car(
+                car.getLicensePlateNumber(),
+                car.getOwnerName(),
+                car.getModel()
+        ));
+    }
+}
