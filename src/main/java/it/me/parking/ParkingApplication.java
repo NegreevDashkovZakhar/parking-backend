2f98db14ef7ce0db3072ddb978c149f2dfdf5cee
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-15 21:51:02 +0600

    [ ]Added javadocs to entry point class

--- src/main/java/it/me/parking/ParkingApplication.java
+++ src/main/java/it/me/parking/ParkingApplication.java
@@ -3,9 +3,17 @@ package it.me.parking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application entry point class
+ */
 @SpringBootApplication
 public class ParkingApplication {
 
+    /**
+     * Application entry point method
+     *
+     * @param args arguments given to application(not used)
+     */
     public static void main(String[] args) {
         SpringApplication.run(ParkingApplication.class, args);
     }
