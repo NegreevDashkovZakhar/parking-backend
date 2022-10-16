cd5614bf0ca6239c253bf37774c9841c3da17ef7
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-16 21:14:01 +0600

    [+]Added parking lot service interface

---
+++ src/main/java/it/me/parking/service/IParkingLotService.java
@@ -0,0 +1,54 @@
+package it.me.parking.service;
+
+import it.me.parking.model.entity.ParkingLot;
+import it.me.parking.model.request.ParkingLotRequest;
+
+/**
+ * Interface for service manipulating parking lot entities in database
+ */
+public interface IParkingLotService {
+    /**
+     * Method getting all parking lots from database
+     *
+     * @return list of all parking lots
+     */
+    Iterable<ParkingLot> getAllParkingLots();
+
+    /**
+     * Method getting parking lot with specified id
+     *
+     * @param id id of the parking lot
+     * @return parking lot with specified id
+     */
+    ParkingLot getParkingLotById(Long id);
+
+    /**
+     * Method updating parking lot properties with specified
+     *
+     * @param id                id of the changed parking lot
+     * @param parkingLotRequest object with new parking lot properties
+     */
+    void updateParkingLot(Long id, ParkingLotRequest parkingLotRequest);
+
+    /**
+     * Method deleting parking lot with specified id
+     *
+     * @param id id of the parking lot
+     */
+    void deleteParkingLotById(Long id);
+
+    /**
+     * Method getting parking lot with specified location
+     *
+     * @param location location of the parking lot
+     * @return parking lot with specified location
+     */
+    ParkingLot getParkingLotByLocation(String location);
+
+    /**
+     * Method adding parking lot to database
+     *
+     * @param parkingLotRequest object with properties of the new parking lot
+     */
+    void addParkingLot(ParkingLotRequest parkingLotRequest);
+}
