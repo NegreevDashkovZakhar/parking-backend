a93c00653202ffcfe689bd9858c74ea5d64ac00d
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-17 21:53:01 +0600

    [+]Added get car by license plate number to repository

--- src/main/java/it/me/parking/repository/CarRepository.java
+++ src/main/java/it/me/parking/repository/CarRepository.java
@@ -1,6 +1,7 @@
 package it.me.parking.repository;
 
 import it.me.parking.model.entity.Car;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,12 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+    /**
+     * Method getting car with specified license plate number
+     *
+     * @param licensePlateNumber license plate number of the car
+     * @return car with specified license plate number
+     */
+    @Query(value = "SELECT * FROM Car WHERE license_plate_number = ?1", nativeQuery = true)
+    Car getCarByLicensePlateNumber(String licensePlateNumber);
 }
