c778769c363cdbf2f025056c1bac175d5b9c95c2
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-17 22:45:09 +0600

    [+]Added reservation service implementation

---
+++ src/main/java/it/me/parking/service/ReservationService.java
@@ -0,0 +1,60 @@
+package it.me.parking.service;
+
+import it.me.parking.model.entity.Reservation;
+import it.me.parking.model.request.ReservationRequest;
+import it.me.parking.repository.ReservationRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Default reservation service interface implementation
+ *
+ * @see it.me.parking.service.IReservationService
+ */
+@Service
+public class ReservationService implements IReservationService {
+    private final ReservationRepository repository;
+
+    /**
+     * Constructor setting used reservation repository
+     *
+     * @param repository reservation repository used for manipulations
+     */
+    public ReservationService(ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Iterable<Reservation> getAllReservations() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Reservation getReservationById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void updateReservation(Long id, ReservationRequest newReservationData) {
+        Reservation oldReservation = repository.findById(id).orElseThrow();
+        oldReservation.setCarId(newReservationData.getCarId());
+        oldReservation.setLotId(newReservationData.getLotId());
+        oldReservation.setStartTime(newReservationData.getStartTime());
+        oldReservation.setEndTime(newReservationData.getEndTime());
+        repository.save(oldReservation);
+    }
+
+    @Override
+    public void deleteReservationById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void addReservation(ReservationRequest reservation) {
+        repository.save(new Reservation(
+                reservation.getCarId(),
+                reservation.getLotId(),
+                reservation.getStartTime(),
+                reservation.getEndTime()
+        ));
+    }
+}
