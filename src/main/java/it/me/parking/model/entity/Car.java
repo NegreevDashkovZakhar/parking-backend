8b0a2689a2d18bc93e48c15c472d46740e9ba067
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-10-21 18:43:14 +0600

    [ ]Changed id generation strategy to identity

--- src/main/java/it/me/parking/model/entity/Car.java
+++ src/main/java/it/me/parking/model/entity/Car.java
@@ -3,6 +3,7 @@ package it.me.parking.model.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.Set;
@@ -13,7 +14,7 @@ import java.util.Set;
 @Entity
 public class Car {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false)
     private String licensePlateNumber;
--- src/main/java/it/me/parking/model/entity/Reservation.java
+++ src/main/java/it/me/parking/model/entity/Reservation.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -16,7 +17,7 @@ import java.util.Date;
 @Entity
 public class Reservation {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(nullable = false)
